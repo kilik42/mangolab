d8bedbdc28fda042bc03ee0eaaa1f25a20078ab1
marvin evins <marvinevins@yahoo.com>
2022-04-03 19:47:17 -0500

    Initial Commit

---
+++ mango/mangoApp.swift
@@ -0,0 +1,17 @@
+//
+//  mangoApp.swift
+//  mango
+//
+//  Created by marvin evins on 4/3/22.
+//
+
+import SwiftUI
+
+@main
+struct mangoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
