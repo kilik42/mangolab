0abe9c81b4633ccfc7d9f7be36a381e1712cf532
marvin evins <marvinevins@yahoo.com>
2022-04-05 21:14:46 -0500

    first commit

--- mango/ContentView.swift
+++ mango/ContentView.swift
@@ -9,8 +9,37 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack(){
+            HStack(){
+                Circle()
+                
+                    .frame(width: 70, height: 70)
+                    .aspectRatio(contentMode: .fill)
+                    .foregroundColor(.green)
+                VStack(alignment: .leading, spacing: 1){
+                    Text("Mango Stack!")
+                        .padding(1)
+                    Text("Mango, Banana Water")
+                        .padding(1)
+                    Text("320 calories")
+                        .padding(1)
+                }
+            }
+            HStack(){
+                ForEach(0..<4){_ in
+                    Circle()
+                    
+                        .frame(width: 40, height: 40)
+                        .aspectRatio(contentMode: .fill)
+                        .foregroundColor(/*@START_MENU_TOKEN@*/.blue/*@END_MENU_TOKEN@*/)
+                }
+            }
+            
+        }
+        
+        
+       
+        
     }
 }
 
